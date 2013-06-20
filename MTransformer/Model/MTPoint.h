e385a490dfec392be02633ed93bdf018ffd93be4
Shuyang Sun <sunbuffett@gmail.com>
2013-06-20 12:54:58 -0500

    Added property declarations.

--- MTransformer/Model/MTPoint.h
+++ MTransformer/Model/MTPoint.h
@@ -8,6 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+@class MTVector; // Define class MTVector
+
+
+/**
+ @interface A MTPoint object that holds a vector representing the posistion in the space, and other data such as the points it connects to, and the visit information for graph triversal.
+ */
 @interface MTPoint : NSObject
+
+/**
+ A vector holding the positon of point in the space.
+ */
+@property (strong, nonatomic) MTVector *vector;
+/**
+ Array of points holding the points it connects to.
+ */
+@property (readonly, strong, nonatomic) NSArray *connectedPoints;
+/**
+ If the point is vistied during the graph traversal.
+ */
+@property (nonatomic) BOOL visited;
 
 @end
