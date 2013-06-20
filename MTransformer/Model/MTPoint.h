f4b174991482d9c3b8e36a852728d8dd0c84b384
Shuyang Sun <sunbuffett@gmail.com>
2013-06-20 09:23:03 -0500

    Added class MTPoint.

---
+++ MTransformer/Model/MTPoint.h
@@ -0,0 +1,13 @@
+//
+//  MTPoint.h
+//  MTransformer
+//
+//  Created by Shuyang Sun on 6/20/13.
+//  Copyright (c) 2013 Shuyang Sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTPoint : NSObject
+
+@end
---
+++ MTransformer/Model/MTPoint.m
@@ -0,0 +1,13 @@
+//
+//  MTPoint.m
+//  MTransformer
+//
+//  Created by Shuyang Sun on 6/20/13.
+//  Copyright (c) 2013 Shuyang Sun. All rights reserved.
+//
+
+#import "MTPoint.h"
+
+@implementation MTPoint
+
+@end
