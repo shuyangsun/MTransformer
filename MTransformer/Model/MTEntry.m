954fd516e7f7cb86bf74431d3f8944b68461930c
Shuyang Sun <sunbuffett@gmail.com>
2013-06-21 17:54:27 -0500

    Added class MTEntry.

---
+++ MTransformer/Model/MTEntry.m
@@ -0,0 +1,13 @@
+//
+//  MTEntry.m
+//  MTransformer
+//
+//  Created by Shuyang Sun on 6/21/13.
+//  Copyright (c) 2013 Shuyang Sun. All rights reserved.
+//
+
+#import "MTEntry.h"
+
+@implementation MTEntry
+
+@end
