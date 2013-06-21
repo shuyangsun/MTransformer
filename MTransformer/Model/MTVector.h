838783f4f9306adb09fdfb4f18e689bee8d2a2ba
Shuyang Sun <sunbuffett@gmail.com>
2013-06-21 17:10:10 -0500

    Added comments.

--- MTransformer/Model/MTVector.h
+++ MTransformer/Model/MTVector.h
@@ -32,112 +32,112 @@
 //************************ Methods ***************************//
 
 /**
- @method Create a vector with certain number of entries, all entries are initialized with 0.
+ Create a vector with certain number of entries, all entries are initialized with 0.
  @param numberOfEntries The number of entries need to be initialized in the vector, including 0 entries.
  */
 -(id)initWithNumberOfEntries: (int) numberOfEntries;
 
 /**
- @method Initialize entries with an array contains all the values.
+ Initialize entries with an array contains all the values.
  @param entriesOfVector  Array containing entries of vector.
  */
 -(id)initWithEntriesArray: (NSArray *) entriesOfVector;
 
 /** 
- @method Get the float value of entry at certain index as a float value.
+ Get the float value of entry at certain index as a float value.
  @param index Index of entry.
  @return The float value of entry.
  */
 -(float)entryAtIndexAsFloat:(int)index;
 
 /**
- @method Get a pointer to a float variable represents the first element of the array, the array is the entries' values. Store, the length in the pointer to len.
+ Get a pointer to a float variable represents the first element of the array, the array is the entries' values. Store, the length in the pointer to len.
  @param arr Address of array, which will store the float values.
  @param len Pointer to NSUInteger variable, which will store the length of arr (number of entries).
  */
 -(void)entriesAsFloatArray: (float *) arr length: (NSUInteger *) len; // C involved.
 
 /**
- @method A method replaces certain entry at index with a float value.
+ A method replaces certain entry at index with a float value.
  @param index The index of entry to replace.
  @param fValue The float number to replace with.
- @return If the operation succeed or not. (If the index value is not valid, it will return false.)
+ @return Whether the operation succeed or not. (If the index value is not valid, it will return false.)
  */
 -(BOOL)replaceEntryAtIndex: (int) index withFloatValue: (float) fValue;
 
 /**
- @method Clear the entries to zero. (Change this vector to a zero vector).
+ Clear the entries to zero. (Change this vector to a zero vector).
  */
 -(void)clearToZeroVector;
 
 /**
- @method Replace current entry value at certain index with number 0.
+ Replace current entry value at certain index with number 0.
  @param index The index of entry to replace.
  */
 -(void)clearEntryAtIndexToZero: (int) index;
 
 /**
- @method Remove the first entry in the vector.
+ Remove the first entry in the vector.
  */
 -(void)removeFirstEntry;
 
 /**
- @method Remove the last entry in the vector.
+ Remove the last entry in the vector.
  */
 -(void)removeLastEntry;
 
 /**
- @method Add an entry with float value to the last rwo of vector.
+ Add an entry with float value to the last rwo of vector.
  @param fValue Float value of entry adding to vector.
  */
 -(void)addEntryWithFloatValue: (float) fValue;
 
 /**
- @method Change current vector to its homogeneous vector.
+ Change current vector to its homogeneous vector.
  */
 -(void)toHomogeneousVector;
 
 /**
- @method Get homogeneous vector of current vector. (make a copy, then add entry with value 1 to the vector)
+ Get homogeneous vector of current vector. (make a copy, then add entry with value 1 to the vector)
  @return The homogeneous vector of current vector.
  */
 -(MTVector *)getHomogeneousVector;
 
 /** 
- @method Remove a certain entry at given index from vector.
+ Remove a certain entry at given index from vector.
  @param index Index of entry to remove.
  */
 -(void)removeEntryAtIndex: (int) index;
 
 /**
- @method Remove entries in certain range from vector.
+ Remove entries in certain range from vector.
  @param range Range of entries to remove.
  */
 -(void)removeEntriesInRange: (NSRange) range;
 
 /**
- @method Get entries in certain range.
+ Get entries in certain range.
  @param range Range of entries want to get.
  @return Array containing entries in range.
  */
--(NSMutableArray *)entriesInRange: (NSRange) range;
+-(NSArray *)entriesInRange: (NSRange) range;
 
 /**
- @method Remove entires not int the range.
+ Remove entires not int the range.
  @param range Range of entries to keep.
  */
 -(void)removeOtherEntiresInVector: (NSRange) range;
 
 /**
- @method Add values in array to vector.
+ Add values in array to vector.
  @param entriesToAdd Enties' value to add to vector.
  */
 -(void)addEntries: (NSArray *) entriesToAdd;
 
 /**
- @method Remove entries under the first two row.
+ Remove entries under the first two row.
  */
--(void)removeEntriesUnderTheFirstTwoRow;
+-(void)removeEntriesUnderTheFirstTwoRows;
 
 //************************ Methods ***************************//
 
--- MTransformer/Model/MTVector.m
+++ MTransformer/Model/MTVector.m
@@ -8,11 +8,11 @@
 
 #import "MTVector.h"
 
-#include "GlobalMacro.h" // Global macro definition.
+#include "GlobalMacro.h" // Global macro definitions.
 
 @interface MTVector()
 
-/** 
+/**
  @method A method generate invalid index warning message.
  @param maxIndex Maximun index value available.
  @param requiredIndex Required index value.
@@ -36,7 +36,7 @@
 				[self addObject:@(ZERO)]; // Initialize with ZEROs.
 			}
 		}
-		self.homogeneous = NO;
+		self.homogeneous = NO; // Initialize proerty homogeneous to NO.
 	}
 	return self;
 }
@@ -59,7 +59,7 @@
 			self = [self initWithNumberOfEntries:1]; // Init as a ZERO vector.
 			NSLog(@"No element in initialize array."); // Generate log message
 		}
-		self.homogeneous = NO;
+		self.homogeneous = NO; // Initialize proerty homogeneous to NO.
 	}
 	return self;
 }
@@ -73,25 +73,25 @@
 // return entry value as float number.
 -(float)entryAtIndexAsFloat:(int)index
 {
-	float res = ZERO;
-	if (index >= 0 && index < [self entryCount]) {
-		res = [[self objectAtIndex:index] floatValue];
+	float res = ZERO; // Initialize result to value ZERO.
+	if (index >= 0 && index < [self entryCount]) { // If it is a valid index:
+		res = [[self objectAtIndex:index] floatValue]; // Get the float value of entry at that index.
 	} else { // If index is invalid, generate log message.
 		[self generateInvalidIndexMessage:[self entryCount] withRequiredIndex:index]; // Generate warning message for invalid index.
 	}
-	return res;
+	return res; // Return the result.
 }
 
 // Return an C float pointer to a float array.
 -(void)entriesAsFloatArray: (float *) arr length: (NSUInteger *) len
 {
 	float *res = NULL; // Initialize res to NULL.
-	for (int i = 0; i < [self entryCount]; ++i) {
+	for (int i = 0; i < [self entryCount]; ++i) { // Iterate through the entries:
 		if ([[self objectAtIndex:i] isKindOfClass:[NSNumber class]]) { // Check if it's a NSNumber.
-			*(res + i) = [[self objectAtIndex:i] floatValue];
+			*(res + i) = [[self objectAtIndex:i] floatValue]; // Set the value in float array to float value of entry.
 		} else { // If it's not an NSNumber, replace value with 0, generate log message.
 			*(res + i) = ZERO; // Assign the value ZERO.
-			NSLog(@"Invalid object in entires.");
+			NSLog(@"Invalid object in entires."); // Generate log message.
 		}
 	}
 	if (arr != NULL) // If arr is not NULL:
@@ -100,59 +100,68 @@
 		*len = (NSUInteger)[self entryCount]; // Get the number of entry.
 }
 
-
+// Change the value of entry at certain index, return operation succeed or not.
 -(BOOL)replaceEntryAtIndex:(int)index withFloatValue:(float)fValue
 {
-	BOOL res = NO;
+	BOOL res = NO; // Initialize the result to be NO, if succeed, change it to YES.
 	if (index >= 0 && index < [self entryCount]) { // If it's a valid index.
-		[self replaceObjectAtIndex:index withObject:@(fValue)];
-		res = YES;
+		[self replaceObjectAtIndex:index withObject:@(fValue)]; // Replace entry with new value.
+		res = YES; // Change the value of result to YES.
 	} else { // If it's not a valid index, generate log message and return false.
 		[self generateInvalidIndexMessage:[self entryCount] withRequiredIndex:index]; // Generate warning message for invalid index.
 	}
-	return res;
+	return res; // Return the result.
 }
 
+// Change the value of entry at certain index to ZERO.
+-(void)clearEntryAtIndexToZero:(int)index
+{
+	[self replaceEntryAtIndex:index withFloatValue:ZERO]; // Replace entry at index with value ZREO.
+}
+
+// Change all entries's value to 0.
 -(void)clearToZeroVector
 {
-	for (int i = 0; i < [self entryCount]; ++i) {
-		[self replaceEntryAtIndex:i withFloatValue:ZERO];
+	for (int i = 0; i < [self entryCount]; ++i) { // Iterate through entries:
+		[self replaceEntryAtIndex:i withFloatValue:ZERO]; // Change the value of entry to ZERO.
 	}
 }
 
--(void)clearEntryAtIndexToZero:(int)index
-{
-	[self replaceEntryAtIndex:index withFloatValue:ZERO];
-}
-
+// Remove the first entry in the vector. (if there are more than one entries)
 -(void)removeFirstEntry
 {
 	if ([self entryCount] > 1) // If there are two or more entries.
-		[self removeObjectAtIndex:0];
+		[self removeObjectAtIndex:0]; // Remove the first entry.
 }
 
+// Remove the last entry in the vector. (if there are more than one entries)
 -(void)removeLastEntry
 {
 	if ([self entryCount] > 1) // If there are two or more entries.
-		[self removeLastObject];
+		[self removeLastObject]; // Remove the last entry.
 }
 
+// Add one entry with given float value to the end of vector.
 -(void)addEntryWithFloatValue:(float)fValue
 {
-	[self addObject:@(fValue)];
+	[self addObject:@(fValue)]; // Add a entry with float value to the end of vector.
 }
 
+// Convert this entry to it's homogeneous vector if it is not.
 -(void)toHomogeneousVector
 {
-	[self addEntryWithFloatValue:ONE];
-	self.homogeneous = YES;
+	if (self.homogeneous == NO) { // Check to see if it's a homogeneous vector.
+		[self addEntryWithFloatValue:ONE]; // If it is not a homogeneous vector, add ONE to the end of vector.
+		self.homogeneous = YES; // Set homogeneous to YES.
+	}
 }
 
+// Return homogeneouse vector of this vector.
 -(MTVector *)getHomogeneousVector
 {
-	MTVector *res = [self mutableCopy];
-	[res toHomogeneousVector];
-	return res;
+	MTVector *res = [self mutableCopy]; // Make a copy of current vector.
+	[res toHomogeneousVector]; // Make the copy homogeneous.
+	return res; // Return the result.
 }
 
 -(void)removeEntryAtIndex:(int)index
@@ -168,37 +177,48 @@
 	}
 }
 
+// Remove entries in certain range.
 -(void)removeEntriesInRange:(NSRange)range
 {
-	[self removeObjectsInRange:range];
+	[self removeObjectsInRange:range]; // Remove objects in this range.
 }
 
--(NSMutableArray *)entriesInRange: (NSRange) range
+// Return an array containing entries in certain range.
+-(NSArray *)entriesInRange: (NSRange) range
 {
-	
-	NSIndexSet *rangeToGet = [NSIndexSet indexSetWithIndexesInRange:range];
-	NSMutableArray *res = (NSMutableArray*)[self objectsAtIndexes:rangeToGet];
-	return res;
+	NSIndexSet *rangeToGet = [NSIndexSet indexSetWithIndexesInRange:range]; // Create a index set of entries in the range.
+	return [self objectsAtIndexes:rangeToGet]; // Return entries in the index set.
 }
 
+// Remove entries beside the given range.
 -(void)removeOtherEntiresInVector:(NSRange)range
 {
-	NSRange firstPartToRemove = NSMakeRange(0, range.location);
-	NSRange secondPartToRemove = NSMakeRange(range.location + range.length, [self entryCount] - (range.location + range.length));
-	[self removeEntriesInRange:firstPartToRemove];
-	[self removeEntriesInRange:secondPartToRemove];
+	NSRange firstPartToRemove = NSMakeRange(0, range.location); // Find the range of the first part to remove.
+	NSRange secondPartToRemove = NSMakeRange(range.location + range.length, [self entryCount] - (range.location + range.length)); // Find the range of the second part to remove.
+	[self removeEntriesInRange:firstPartToRemove]; // Remove the first part.
+	[self removeEntriesInRange:secondPartToRemove]; // Remove the second part.
 }
 
+// Add entries to the end of the array.
 -(void)addEntries:(NSArray *)entriesToAdd
 {
-	[self addObjectsFromArray:entriesToAdd];
+	if (self.homogeneous == NO){ // If it is NOT the homogeneous form, just add entries to the end of the vector.
+		[self addObjectsFromArray:entriesToAdd]; // Add entries to the end of the vector.
+	} else { // If it is the homogeneous form, insert entries above the last entry.
+		NSIndexSet *indexes = [NSIndexSet indexSetWithIndex:[self count] -1]; // Create a index set, contain only the last index of this vector.
+		[self insertObjects:entriesToAdd atIndexes:indexes]; // Add entries to the end of vector, above the last one.
+	}
 }
 
--(void)removeEntriesUnderTheFirstTwoRow
+//************************ For 3D transformation ***************************//
+
+// Get the 2D projection vectors. Also remove the last entry if it is a homogeneous vector.
+-(void)removeEntriesUnderTheFirstTwoRows
 {
-	if ([self entryCount] > 2)
-		[self removeObjectsInRange:NSMakeRange(2, [self entryCount] - 2)];
+	if ([self entryCount] > 2) // If there are more than two entries:
+		[self removeObjectsInRange:NSMakeRange(2, [self entryCount] - 2)]; // Remove all entries under the first two rows.
 }
+//************************ For 3D transformation ***************************//
 
 // Overriding entryCount getter method.
 -(NSUInteger)entryCount
@@ -209,33 +229,35 @@
 // Overriding dimension getter method.
 -(NSUInteger)dimension
 {
-	unsigned long res = 0;
-	for (int i = 0; i < [self entryCount]; ++i) {
-		if ([[self objectAtIndex:i] isKindOfClass:[NSNumber class]] && [[self objectAtIndex:i] floatValue] != ZERO) {
-			++res;
+	NSUInteger res = 0; // Create a result to return later, initialize it to 0.
+	for (int i = 0; i < [self entryCount]; ++i) { // Iterate through the loop, find none ZERO vectors.
+		if ([[self objectAtIndex:i] isKindOfClass:[NSNumber class]] && [[self objectAtIndex:i] floatValue] != ZERO) { // If current entry is a NSNumber and is a none ZERO vector.
+			++res; // Add one to res.
 		}
 	}
-	return res;
+	return res; // Return the result.
 }
 
 // Overriding description
 -(NSString *)description
 {
-	NSMutableString *res = [NSMutableString stringWithFormat:@"%uD vector: (", [self entryCount]];
-	for (int i = 0; i < [self entryCount]; ++i) {
-		NSMutableString *appending = [NSMutableString stringWithFormat:@"%.1f", [[self objectAtIndex:i] floatValue]];
+	NSMutableString *res = [NSMutableString stringWithFormat:@"%uD vector: (", [self entryCount]]; // Description should contain the dimension of vector, and entries.
+	for (int i = 0; i < [self entryCount]; ++i) { // Loop through all the entries:
+		NSMutableString *appending = [NSMutableString stringWithFormat:@"%.1f", [[self objectAtIndex:i] floatValue]]; // Append the value of entry.
 		if (i < [self entryCount] - 1) { // If this is not the last element, add coma and space.
-			[appending appendString:@", "];
+			[appending appendString:@", "]; // Append comma and space.
 		}
-		[res appendString:appending];
+		[res appendString:appending]; // Append the entries information string.
+		appending = nil; // Set appending to nil.
 	}
-	return res;
+	[res appendString:@")"]; // Add close parenthesize.
+	return res; // Return the result.
 }
 
 // Generate warning indicates invalid index.
 -(void)generateInvalidIndexMessage:(NSUInteger)maxIndex withRequiredIndex:(int)requiredIndex
 {
-	NSLog(@"Invalid index.\nentires length: %u\nrequested index: %d", maxIndex, requiredIndex); // Generate log message.
+	NSLog(@"Invalid index.\nentires length: %u\nrequested index: %d", maxIndex, requiredIndex); // Generate log message, containing max index and required index.
 }
 
 @end
