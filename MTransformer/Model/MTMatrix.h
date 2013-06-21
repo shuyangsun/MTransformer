2ab1d34725d722c122bf40cc12a4e651d2c727bd
Shuyang Sun <sunbuffett@gmail.com>
2013-06-21 09:03:45 -0500

    Added class: MTMatrix.

---
+++ MTransformer/Model/MTMatrix.h
@@ -0,0 +1,13 @@
+//
+//  MTMatrix.h
+//  MTransformer
+//
+//  Created by Shuyang Sun on 6/21/13.
+//  Copyright (c) 2013 Shuyang Sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MTMatrix : NSObject
+
+@end
