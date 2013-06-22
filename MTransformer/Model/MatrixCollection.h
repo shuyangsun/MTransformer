041e5fb004a0346b7b94a91b75e04d2c7cd2020f
Shuyang Sun <sunbuffett@gmail.com>
2013-06-22 18:18:53 -0500

    Added matrix collection, get predefined matrices.

---
+++ MTransformer/Model/MatrixCollection.h
@@ -0,0 +1,13 @@
+//
+//  MatrixCollection.h
+//  MTransformer
+//
+//  Created by Shuyang Sun on 6/22/13.
+//  Copyright (c) 2013 Shuyang Sun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MatrixCollection : NSObject
+
+@end
---
+++ MTransformer/Model/MatrixCollection.m
@@ -0,0 +1,13 @@
+//
+//  MatrixCollection.m
+//  MTransformer
+//
+//  Created by Shuyang Sun on 6/22/13.
+//  Copyright (c) 2013 Shuyang Sun. All rights reserved.
+//
+
+#import "MatrixCollection.h"
+
+@implementation MatrixCollection
+
+@end
