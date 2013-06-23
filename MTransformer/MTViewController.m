c0c46d76f2db18642f227ad2c06a06c1339d3386
Shuyang Sun <sunbuffett@gmail.com>
2013-06-23 15:44:06 -0500

    Comments changed.

--- MTransformer/MTViewController.m
+++ MTransformer/MTViewController.m
@@ -21,19 +21,6 @@
 -(void)setup
 {
 	// Initialization code here...
-	MTMatrix *matrix = [[MatrixCollection sharedCollection] translateTransformationMatrixWith_xValue:2
-																						  and_yValue:3
-																						  and_zValue:4];
-
-	NSMutableString *matrixDescription = [NSMutableString string];
-	for (size_t i = 0; i < [matrix.vectors[0] entryCount]; ++i) {
-		for (size_t j = 0; j < [matrix.vectors count]; ++j) {
-			[matrixDescription appendString:[NSString stringWithFormat:@"%.1f ", [(NSNumber*)matrix.vectors[j][i] floatValue]]];
-			printf("%.1f ", [(NSNumber*)matrix.vectors[j][i] floatValue]);
-		}
-		[matrixDescription appendString:@"\n"];
-		putchar('\n');
-	}
 }
 
 -(void)awakeFromNib
